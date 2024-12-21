35a4248fb5fb4a82c560bd1cd1b3fda559cebf3b
Luc AUCOIN <luc.aucoin1998@gmail.com>
2024-12-21 11:18:19 +0100

    fix(auth): keycloak error management

--- src/main/kotlin/fr/laucoin/registry/backend/infrastructure/external/keycloak/adapter/KeycloakAuthenticationAdapter.kt
+++ src/main/kotlin/fr/laucoin/registry/backend/infrastructure/external/keycloak/adapter/KeycloakAuthenticationAdapter.kt
@@ -1,7 +1,8 @@
 package fr.laucoin.registry.backend.infrastructure.external.keycloak.adapter
 
-import fr.laucoin.registry.backend.domain.constant.ErrorConst.AuthError.AUTH_PROVIDER_4XX_ERROR
+import fr.laucoin.registry.backend.domain.constant.ErrorConst.AuthError.AUTHORIZATION_CODE_OUTDATED
 import fr.laucoin.registry.backend.domain.constant.ErrorConst.AuthError.AUTH_PROVIDER_FAILED
+import fr.laucoin.registry.backend.domain.constant.ErrorConst.AuthError.REFRESH_TOKEN_OUTDATED
 import fr.laucoin.registry.backend.domain.model.AuthenticationUriModel
 import fr.laucoin.registry.backend.domain.model.RegistryExceptionModel
 import fr.laucoin.registry.backend.domain.model.TokenModel
@@ -9,8 +10,8 @@ import fr.laucoin.registry.backend.domain.port.IAuthenticationPort
 import fr.laucoin.registry.backend.infrastructure.external.keycloak.entity.KeycloakTokenEntity
 import fr.laucoin.registry.backend.infrastructure.external.keycloak.mapper.AuthenticationTokenEntityMapper
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.FAILED_DEPENDENCY
+import org.springframework.http.HttpStatus.UNAUTHORIZED
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.BodyInserters.FormInserter
@@ -54,18 +55,20 @@ class KeycloakAuthenticationAdapter(
         return fetchToken(
             BodyInserters.fromFormData("grant_type", "authorization_code")
                 .with("code", authorizationCode)
-                .with("redirect_uri", redirectUri)
+                .with("redirect_uri", redirectUri),
+            AUTHORIZATION_CODE_OUTDATED
         )
     }
 
     override fun refreshAuthenticationToken(refreshToken: String): Mono<TokenModel> {
         return fetchToken(
             BodyInserters.fromFormData("grant_type", "refresh_token")
-                .with("refresh_token", refreshToken)
+                .with("refresh_token", refreshToken),
+            REFRESH_TOKEN_OUTDATED
         )
     }
 
-    private fun fetchToken(body: FormInserter<String>): Mono<TokenModel> {
+    private fun fetchToken(body: FormInserter<String>, errorMessage: String): Mono<TokenModel> {
         return http.post().uri("$baseUri$TOKEN_PATH")
             .body(
                 body
@@ -75,7 +78,7 @@ class KeycloakAuthenticationAdapter(
             .retrieve()
             .onStatus(
                 { it.is4xxClientError },
-                { Mono.error(RegistryExceptionModel(HttpStatus.valueOf(it.statusCode().value()), AUTH_PROVIDER_4XX_ERROR)) })
+                { Mono.error(RegistryExceptionModel(UNAUTHORIZED, errorMessage)) })
             .onStatus({ it.is5xxServerError }, { Mono.error(RegistryExceptionModel(FAILED_DEPENDENCY, AUTH_PROVIDER_FAILED)) })
             .bodyToMono(KeycloakTokenEntity::class.java)
             .map(mapper::toModel)
